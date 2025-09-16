170614372fedd241e4101424c084e79d67ad728a
Sushma Kanchi <kasushma@deloitte.com>
2025-09-16 08:50:32 +0000

    rest

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,9 @@
+@protocol : 'rest'
 service Test {
 function printhello ( input : String) returns String;
+function addition ( num1 : Integer, num2: Integer) returns Integer;
+
+@open
+type object{};
+function NewFunction ( Category : Integer) returns object;
 }
--- srv/service.js
+++ srv/service.js
@@ -1,9 +1,36 @@
 const cds = require("@sap/cds");
 
-module.exports = cds.service.impl( async function(srv) {
+module.exports = cds.service.impl(async function (srv) {
     srv.on('printhello', req => {
-
         console.log(req.data.input) //print the request coming from server
         return `${req.data.input} world`
-    } )
-} )
+    })
+
+    srv.on('addition', req => {
+        console.log(req.data)
+        let res = req.data.num1 + req.data.num2;
+        return res
+
+    })
+
+    srv.on('MyFunction', req => {
+        let result = {}
+        if (req.data.category == 1) {
+            result.stock = 120,
+                result.product = 'Thar',
+                result.price = '12879 USD',
+                result.type = 'Car'
+        }
+        else {
+            result.stock = 150,
+                result.product = 'Benz',
+                result.price = '1900 USD',
+                result.Array = {
+                    'Type': 'Car',
+                    'Discount': '35 %'
+                }
+        }
+        console.log(result);
+        return result
+    })
+})
